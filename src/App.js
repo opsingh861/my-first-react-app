3060ac61b05e1e300cca0f44128846b196b85075
opsingh861 <62853140+opsingh861@users.noreply.github.com>
2023-07-21 13:41:51 +0530

    Hooks and Props done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import ClassComponents from './components/ClassComponents';
 import FunctionalComponents from './components/FunctionalComponents';
-
+import { useState } from 'react';
+const surname = "Dhanraj"
 function App() {
+  const [name, setName] = useState("Aditya")
+
   return (
     <div className="App">
       <h1>Class Component</h1>
       <ClassComponents />
       <br />
       <h1>Functional Components</h1>
-      <FunctionalComponents />
+      <FunctionalComponents name={name} surname={surname} setName={setName} />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/FunctionalComponents.jsx
+++ src/components/FunctionalComponents.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FunctionalComponents = () => {
+const FunctionalComponents = (props) => {
+    let [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("")
+    const subtraction = () => {
+        setCount(--count)
+    }
     return (
-        <p>This is Functional Component</p>
+        <div>
+            <p>This is Functional Component</p>
+            <button onClick={() => setCount(++count)}>Click to add one</button>
+            <button onClick={subtraction}>Click to subtract one</button>
+            <h1>{count}</h1>
+
+            <h1>My name is {props.name} {props.surname} </h1>
+            <h1>{props.count}</h1>
+            <input type="text" onChange={(e) => setChangeName(e.target.value)} />
+            <button onClick={() => props.setName(changeName)}>Click on the button to change the name</button>
+        </div>
+
     )
 }
 
